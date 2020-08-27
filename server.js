0205686a2fb7fa45320040d0a55a373b7ee7ecb3
Tania Sainvilus <taniasainvilus@Tanias-Air.hsd1.ga.comcast.net>
2020-08-27 12:10:34 -0400

    cors error

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(
   })
 )
 
-const whitelist = ['http://localhost:3000', 'http://ventdchatapp-frontend.herokuapp.com/', 'https://ventdchatapp-frontend.herokuapp.com'];
+const whitelist = ['http://localhost:3000', 'https://ventdchatapp-frontend.herokuapp.com'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
