6781ae297a52745a05c4c4bab19d19cfce0cd1be
Tania Sainvilus <taniasainvilus@Tanias-Air.hsd1.ga.comcast.net>
2020-08-28 13:57:30 -0400

    added access control origin to socket.io

--- server.js
+++ server.js
@@ -1,46 +1,50 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const session = require('express-session')
+// const MongoStore = require('connect-mongo')(session)
 const cors = require('cors')
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = module.exports.io = require('socket.io')(http, { origins: '*:*'})
+// const sharedsession = require('express-socket.io-session');
 const PORT = process.env.PORT || 3003
 
-const mongodbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ventd'
-console.log(mongodbURI)
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ventd'
 require('dotenv').config()
+
+
 
 // Error / Disconnection
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
 //Database Connection
-mongoose.connect(mongodbURI, { 
+mongoose.connect(mongoURI, { 
   useNewUrlParser: true, 
   useUnifiedTopology: true,
-  useCreateIndex: true,
+  useCreateIndex: true
 })
 mongoose.connection.once('open', ()=>{
     console.log('connected to mongoose...')
 })
 
 //middleware
-app.use(express.json())
-app.use(
-  session({
-    secret: process.env.JWT_SECRET,
-    resave: false, 
-    saveUninitialized: false 
-  })
-)
+// app.use(express.json())
+// app.use(
+//   session({
+//     secret: process.env.JWT_SECRET, 
+//     resave: false, 
+//     saveUninitialized: false,
+//     // store: new MongoStore({ mongooseConnection: mongoose.connection })
+//   })
+// )
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://ventdchatapp-frontend.herokuapp.com');
   next();
 });
 
-const whitelist = ['http://localhost:3000', 'https://ventdchatapp-frontend.herokuapp.com', 'https://ventdchatapp-frontend.herokuapp.com/'];
+const whitelist = ['http://localhost:3000', 'https://ventdchatapp-frontend.herokuapp.com/', 'https://ventdchatapp-frontend.herokuapp.com'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -52,13 +56,18 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 
+//For allowing all headers to avoid preflight CORS problems
+// app.all((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://ventdchatapp-frontend.herokuapp.com');
+//   next();
+// });
 
 //Controller/Routes
 const chatsController = require("./controllers/chat.js");
 app.use("/chats", chatsController);
 
-const userController = require('./controllers/userRouter.js')
-app.use('/user', userController)
+// const userController = require('./controllers/userRouter.js')
+// app.use('/user', userController)
 
 app.get('/', (req, res) => {
   res.redirect('/');
@@ -66,8 +75,9 @@ app.get('/', (req, res) => {
 
 const Chat = require("./models/chat.js");
 
-io.on('connection', (socket) => {
+// io.use(sharedsession(session));
 
+io.on('connection', (socket) => {
   // Get the last 10 messages from the database.
   Chat.find().sort({createdAt: -1}).limit(10).exec((err, chat) => {
     if (err) return console.error(err);
